fc98e350bbb432b8886b7e61ae2b00f772299fcb
Pedro Henrique <pedrohenrique.santos.dev@gmail.com>
2026-09-15 14:02:42 -0300

    primeiro commit

---
+++ src/main/java/com/example/whorkshopmongo/WhorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.whorkshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhorkshopmongoApplication.class, args);
+	}
+
+}
