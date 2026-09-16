d431e4100a0ac9ca48f51c967e00e39c07af9db7
Pedro Henrique <pedrohenrique.santos.dev@gmail.com>
2026-09-16 15:03:41 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/example/whorkshopmongo/config/Instatiation.java
+++ src/main/java/com/example/whorkshopmongo/config/Instatiation.java
@@ -1,12 +1,16 @@
 package com.example.whorkshopmongo.config;
 
+import com.example.whorkshopmongo.domain.Post;
 import com.example.whorkshopmongo.domain.User;
+import com.example.whorkshopmongo.repositories.PostRepository;
 import com.example.whorkshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instatiation implements CommandLineRunner {
@@ -14,15 +18,23 @@ public class Instatiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
-        userRepository.deleteAll();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
--- src/main/java/com/example/whorkshopmongo/domain/User.java
+++ src/main/java/com/example/whorkshopmongo/domain/User.java
@@ -2,8 +2,11 @@ package com.example.whorkshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
