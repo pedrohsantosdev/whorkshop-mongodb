92fcb1314528086210c6a4485a40624e861549f8
Pedro Henrique <pedrohenrique.santos.dev@gmail.com>
2026-09-17 13:59:35 -0300

    Consulta com vários critérios

--- src/main/java/com/example/whorkshopmongo/services/PostService.java
+++ src/main/java/com/example/whorkshopmongo/services/PostService.java
@@ -5,6 +5,7 @@ import com.example.whorkshopmongo.repositories.PostRepository;
 import com.example.whorkshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,11 +20,15 @@ public class PostService {
 
     public Post findById(String id) {
         Optional<Post> obj = postRepository.findById(id);
-        return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
     public List<Post> findByTitle(String text) {
         return postRepository.findByTitleContainingIgnoreCase(text);
     }
 
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
+    }
 }
